c104f4847459aaa54fc384fcfe3399d2d20c1002
Marcus Parmentier and Saneyee Sarkar <marcus@>
2017-11-02 16:57:16 -0700

    Add our database

--- my_library (1).sql
+++ my_library (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 02, 2017 at 01:52 AM
+-- Generation Time: Nov 03, 2017 at 12:55 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -40,7 +40,8 @@ CREATE TABLE `authors` (
 INSERT INTO `authors` (`id`, `author_name`) VALUES
 (1, 'Shakespeare'),
 (2, 'Jane austen'),
-(3, 'Rudyard Kipling');
+(3, 'Rudyard Kipling'),
+(4, 'Rabindranath Tagore');
 
 -- --------------------------------------------------------
 
@@ -62,7 +63,9 @@ INSERT INTO `books` (`id`, `title`, `genre`) VALUES
 (1, 'Robinhood', 'Fiction'),
 (2, 'Cindrella', 'Fantasy'),
 (3, 'Harry', 'Fantasy'),
-(4, 'Bella', 'Fantasy');
+(4, 'Bella', 'Fantasy'),
+(5, 'Jurassic Park', 'Sci/Fic'),
+(6, 'Gone With The Wind', 'Romance');
 
 -- --------------------------------------------------------
 
@@ -81,13 +84,52 @@ CREATE TABLE `books_authors` (
 --
 
 INSERT INTO `books_authors` (`id`, `book_id`, `author_id`) VALUES
-(5, 0, 1),
-(6, 0, 2),
-(7, 2, 0),
-(8, 1, 0),
-(9, 0, 1),
-(10, 1, 0),
-(11, 1, 0);
+(31, 1, 1),
+(32, 3, 1),
+(33, 4, 1),
+(34, 1, 2),
+(35, 3, 3),
+(36, 1, 1),
+(37, 5, 2),
+(38, 5, 2),
+(39, 5, 1),
+(40, 5, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkouts`
+--
+
+CREATE TABLE `checkouts` (
+  `id` int(11) NOT NULL,
+  `copy_id` int(11) DEFAULT NULL,
+  `patron_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `copies`
+--
+
+CREATE TABLE `copies` (
+  `id` int(11) NOT NULL,
+  `numof_copies` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrons`
+--
+
+CREATE TABLE `patrons` (
+  `id` int(11) NOT NULL,
+  `patron_name` varchar(255) NOT NULL,
+  `checkout_date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -115,6 +157,24 @@ ALTER TABLE `books_authors`
   ADD UNIQUE KEY `id` (`id`);
 
 --
+-- Indexes for table `checkouts`
+--
+ALTER TABLE `checkouts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `copies`
+--
+ALTER TABLE `copies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patrons`
+--
+ALTER TABLE `patrons`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -122,17 +182,32 @@ ALTER TABLE `books_authors`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `books_authors`
 --
 ALTER TABLE `books_authors`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+--
+-- AUTO_INCREMENT for table `checkouts`
+--
+ALTER TABLE `checkouts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `copies`
+--
+ALTER TABLE `copies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `patrons`
+--
+ALTER TABLE `patrons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
